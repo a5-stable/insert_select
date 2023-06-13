212de6c022b7b94bb52581daa354374e19f7d368
a5-stable <sh07e1916@gmail.com>
2023-06-13 21:01:27 +0900

    delegate to builder

--- lib/insert_select/active_record.rb
+++ lib/insert_select/active_record.rb
@@ -56,13 +56,105 @@ module InsertSelect
 
       class Builder
         attr_reader :relation, :constant, :mapping, :returning, :record_timestamps
+
         def initialize(insert_select_from)
-          @relation = insert_select_from.relation
-          @constant = insert_select_from.constant
-          @mapping = insert_select_from.mapping
+          @relation = insert_select_from.relation.all
+          @constant = insert_select_from.constant || {}
+          @mapping = insert_select_from.mapping || {}
           @returning = insert_select_from.returning
           @record_timestamps = insert_select_from.record_timestamps
+          @connection = insert_select_from.connection
         end
+
+        def mapper
+          result = {}
+          insert_mapping = mapping.transform_keys(&:to_s)
+          mapping_column_names = mapping.keys
+
+          if selected_column_names.present?
+            selected_column_names.each do |column_name|
+              if mapping[column_name]
+                insert_mapping[column_name.to_s] = mapping[column_name]
+                mapping_column_names.delete(column_name)
+              else
+                insert_mapping[column_name.to_s] = column_name
+              end
+            end
+          end
+
+          if mapping_column_names.present?
+            @connection.columns(relation.table_name).map{|c|
+              insert_mapping[c.name.to_s] = mapping[c.name.to_sym] || c.name
+            }
+          end
+
+          constant_mapping = {}
+          if insert_mapping.blank?
+            @connection.columns(relation.table_name).map{|c|
+              insert_mapping[c.name.to_s] = c.name
+            }
+          end
+
+          constant.each {|k, v|
+            insert_mapping.delete(k.to_s)
+            relation.select_values = relation.select_values - [k.to_sym]
+            constant_mapping[k.to_s] = "\"#{v}\""
+          }
+
+          result[:insert_mapping] = insert_mapping
+          result[:constant_mapping] = constant_mapping
+
+          result
+        end
+
+        # def insert_mapping
+        #   res = mapping.transform_keys(&:to_s)
+        #   mapping_column_names = mapping.keys
+
+        #   if selected_column_names.present?
+        #     selected_column_names.each do |column_name|
+        #       if mapping[column_name]
+        #         res[column_name.to_s] = mapping[column_name]
+        #         mapping_column_names.delete(column_name)
+        #       else
+        #         res[column_name.to_s] = column_name
+        #       end
+        #     end
+        #   end
+
+        #   if mapping_column_names.present?
+        #     @connection.columns(relation.table_name).map{|c|
+        #       res[c.name.to_s] = mapping[c.name.to_sym] || c.name
+        #     }
+        #   end
+
+        #   res
+        # end
+
+        # def constant_mapping
+        #   res = {}
+        #   insert_mapping = insert_mapping || {}
+        #   if insert_mapping.blank?
+        #     @connection.columns(relation.table_name).map{|c|
+        #       insert_mapping[c.name.to_s] = c.name
+        #     }
+        #   end
+
+        #   constant.each {|k, v|
+        #     insert_mapping.delete(k.to_s)
+        #     relation.select_values = relation.select_values - [k.to_sym]
+        #     res[k.to_s] = "\"#{v}\""
+        #   }
+
+        #   res
+        # end
+
+        def selected_column_names
+          relation.select_values
+        end
+
+        private
+
       end
     end
   end
--- lib/insert_select/adapters/base_adapter.rb
+++ lib/insert_select/adapters/base_adapter.rb
@@ -3,62 +3,35 @@ module InsertSelect
     require "insert_select/errors"
 
     class BaseAdapter
+      attr_reader :table_name, :connection
       def initialize(table_name, connection)
         @table_name = table_name
         @connection = connection
       end
 
-      def build_sql(insert_select_from)
-        mapping = insert_select_from.mapping || {}
-        constant = insert_select_from.constant || {}
-        relation = insert_select_from.relation.all
+      def build_sql(builder)
+        relation = builder.relation.all
+        mapper = builder.mapper
+        insert_mapping = mapper[:insert_mapping]
+        constant_mapping = mapper[:constant_mapping]
+        quoted_table_name = @connection.quote_table_name(table_name)
 
-        insert_mapping = mapping.transform_keys(&:to_s)
-        selected_column_names = relation.select_values
-        mapping_column_names = mapping.keys
-
-        if selected_column_names.present?
-          selected_column_names.each do |column_name|
-            if mapping[column_name]
-              insert_mapping[column_name.to_s] = mapping[column_name]
-              mapping_column_names.delete(column_name)
-            else
-              insert_mapping[column_name.to_s] = column_name
-            end
-          end
-        end
-
-        if mapping_column_names.present?
-          @connection.columns(relation.table_name).map{|c|
-            insert_mapping[c.name.to_s] = mapping[c.name.to_sym] || c.name
-          }
-        end
-
-        if constant.present?
-          if insert_mapping.blank?
-            @connection.columns(relation.table_name).map{|c|
-              insert_mapping[c.name.to_s] = c.name
-            }
-          end
-
-          constant.each {|k, v|
-            insert_mapping.delete(k.to_s)
-            relation.select_values = relation.select_values - [k.to_sym]
-            insert_mapping["\"#{v}\""] = k
-          }
-        end
-
-        quoted_table_name = @connection.quote_table_name(@table_name)
-
-        if insert_mapping.present?
+        if insert_mapping.present? || constant_mapping.present?
           c = []
           insert_mapping.each {|k, v|
             c << v
-            relation = relation.select(k.to_s) if selected_column_names.map(&:to_s).exclude?(k.to_s)
+            relation = relation.select(k.to_s) if builder.selected_column_names.map(&:to_s).exclude?(k.to_s)
+          }
+
+          constant_mapping.each {|k, v|
+            c << k
+            relation.select_values = relation.select_values - [k.to_sym]
+            relation = relation.select(v)
           }
           stmt = "INSERT INTO #{quoted_table_name} "
           stmt += "(#{c.join(', ')}) "
           stmt += relation.to_sql
+
           stmt
         else
           stmt = "INSERT INTO #{quoted_table_name} #{relation.to_sql}"
