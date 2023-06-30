94af9e8c0b9ca4b32ec2d19c85637ebb7e715feb
a5-stable <sh07e1916@gmail.com>
2023-07-01 00:15:14 +0900

    fix spec

--- spec/insert_select_spec.rb
+++ spec/insert_select_spec.rb
@@ -141,11 +141,11 @@ RSpec.describe InsertSelect do
       skip "returning not supported" unless ActiveRecord::Base.connection.supports_insert_returning?
 
       res = NewUserWithSameColumn.insert_select_from(OldUser, returning: :id)
-      expect(res.pluck(:id)).to eq([1, 2, 3, 4, 5, 6])
+      expect(res.pluck("id")).to eq([1, 2, 3, 4, 5, 6])
 
       res = NewUserWithSameColumn.insert_select_from(OldUser, returning: %i[id name])
-      expect(res.pluck(:id)).to eq([1, 2, 3, 4, 5, 6])
-      expect(res.pluck(:name)).to eq(["Dave", "Dee", "Dozy", "Beaky", "Mick", "Tich"])
+      expect(res.pluck("id")).to eq([1, 2, 3, 4, 5, 6])
+      expect(res.pluck("name")).to eq(["Dave", "Dee", "Dozy", "Beaky", "Mick", "Tich"])
     end
 
     it "can skip duplicate data with not bang method " do
